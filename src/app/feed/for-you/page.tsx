2337f42c10b9ba2fe8953d994c6635c1bfab4b84
dimitarspiroski <dimitarspiroski997@gmail.com>
2025-10-17 10:49:51 +0200

    changes to feed and slot route compose

--- src/app/feed/for-you/page.tsx
+++ src/app/feed/for-you/page.tsx
@@ -1,9 +1,11 @@
+export const dynamic = "force-dynamic";
+
 import ComposeTweet from "@/components/compose-tweet";
 import Tweets from "@/components/tweets";
 import { TweetExtendedModel } from "@/db/schemas/tweet.schema";
 import { getTweets } from "@/services/tweets.service";
 
-export default async function ForYour() {
+export default async function ForYou() {
   const tweets = await getTweets();
 
   return (
