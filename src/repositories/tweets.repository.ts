0bef55e1081304404cf40175a3b2583ded3ff54c
dimitarspiroski <dimitarspiroski997@gmail.com>
2025-12-27 14:19:18 +0100

    logging error on repo function

--- src/repositories/tweets.repository.ts
+++ src/repositories/tweets.repository.ts
@@ -24,6 +24,7 @@ export const find = (searchTerm: string | null): Promise<TweetModel[]> => {
       },
     });
   } catch (error) {
+    console.log(error);
     return Promise.resolve([]);
   }
 };
